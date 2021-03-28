c4155a417f19590715316ec44b1fad7309951f0a
Evan Xu <evan.qin.xu@gmail.com>
2021-03-28 14:53:55 +1100

    Add two more parts: linear data structures and non-linear data structures.

---
+++ Linear Data Structures/LinearDataStructures/src/com/evanxu/Main.java
@@ -0,0 +1,10 @@
+package com.evanxu;
+
+public class Main {
+    public static void main(String[] names) {
+        var array = new Array(10);
+        array.insert(10);
+        System.out.println(array.indexOf(10));
+    }
+}
+
---
+++ Non-linear Data Structures/NonLinearDataStructures/src/com/evanxu/Main.java
@@ -0,0 +1,15 @@
+package com.evanxu;
+
+public class Main {
+    public static void main(String[] names) {
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.print();
+
+    }
+}
+
